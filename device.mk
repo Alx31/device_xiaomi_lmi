d7e34160eb56181eb17e463b0411dd74601086d7
dataoutputstream <mb150996@gmail.com>
2020-10-07 12:17:54 +0200

    lmi: Import audio policy and build tinymix
    Change-Id: I3ebce4461092990f496c2b02a372c0adeabce840

--- device.mk
+++ device.mk
@@ -41,6 +41,15 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    libaacwrapper \
+    tinymix
+
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/audio/audio_policy_engine_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_engine_configuration.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
