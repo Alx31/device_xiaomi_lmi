1a61657aa154bea683f45e02481f2d5f6c90c1d2
Alx31 <alex31.couderc@gmail.com>
2020-10-25 14:19:17 +0100

    tipsy

--- tipsy_lmi.mk
+++ tipsy_lmi.mk
@@ -7,11 +7,11 @@
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Tipsy stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_lmi
+PRODUCT_NAME := tipsy_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_BRAND := POCO
 PRODUCT_MANUFACTURER := Xiaomi
