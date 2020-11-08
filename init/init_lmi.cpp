ca1af4b15ad5ca43d09706da48f9eaaa3ef54a1f
Arian <arian.kulmer@web.de>
2020-11-08 18:06:00 +0100

    lmi: Workaround safetynet

--- init/init_lmi.cpp
+++ init/init_lmi.cpp
@@ -14,7 +14,10 @@
  * limitations under the License.
  */
 
-#include <android-base/logging.h>
+#include <fstream>
+#include <unistd.h>
+#include <vector>
+
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
@@ -22,42 +25,85 @@
 #include "property_service.h"
 #include "vendor_init.h"
 
+using android::base::GetProperty;
 using android::init::property_set;
 
-void property_override(char const prop[], char const value[])
+std::vector<std::string> ro_props_default_source_order =
+{
+    "",
+    "bootimage.",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+};
+
+void property_override(char const prop[], char const value[], bool add = true)
 {
     prop_info *pi;
 
-    pi = (prop_info*) __system_property_find(prop);
+    pi = (prop_info *) __system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
-    else
+    else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
-void load_lmiglobal() {
-    property_override("ro.product.model", "Poco F2 Pro");
-    property_override("ro.build.product", "lmi");
-    property_override("ro.product.device", "lmi");
-    property_override("ro.build.description", "Redmi/lmi/lmi:10/QKQ1.191117.002/V12.0.4.0.QJKMIXM:user/release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-}
-void load_lmi() {
-    property_override("ro.product.model", "Redmi K30 Pro");
-    property_override("ro.build.product", "lmi");
-    property_override("ro.product.device", "lmi");
-    property_override("ro.build.description", "Redmi/lmi/lmi:10/QKQ1.191117.002/V12.0.4.0.QJKMIXM:user/release-keys");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
-}
+void set_ro_build_prop(const std::string &prop, const std::string &value)
+{
+    for (const auto &source : ro_props_default_source_order) {
+        auto prop_name = "ro." + source + "build." + prop;
+        if (source == "")
+            property_override(prop_name.c_str(), value.c_str());
+        else
+            property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
+
+void set_ro_product_prop(const std::string &prop, const std::string &value)
+{
+    for (const auto &source : ro_props_default_source_order) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
 
 void vendor_load_properties() {
-    std::string region = android::base::GetProperty("ro.boot.hwc", "");
+    std::string region;
+    region = GetProperty("ro.boot.hwc", "GLOBAL");
 
-    if (region.find("CN") != std::string::npos) {
-        load_lmi();
-    } else if (region.find("GLOBAL") != std::string::npos) {
-        load_lmiglobal();
-    } else {
-        LOG(ERROR) << __func__ << ": unexcepted region!";
+    std::string model;
+    std::string device;
+    std::string fingerprint;
+    std::string description;
+    std::string mod_device;
+
+    if (region == "GLOBAL")
+    {
+        model = "Poco F2 Pro";
+        device = "lmi";
+        fingerprint = "Redmi/lmi/lmi:10/QKQ1.191117.002/V12.0.4.0.QJKMIXM:user/release-keys";
+        description = "lmi-user 10 QKQ1.191117.002 V12.0.4.0.QJKMIXM release-keys";
+        mod_device = "lmi_global";
+    }
+    else if (region == "CN")
+    {
+        model = "Redmi K30 Pro";
+        device = "lmi";
+        fingerprint = "Redmi/lmi/lmi:10/QKQ1.191117.002/V12.0.4.0.QJKMIXM:user/release-keys";
+        description = "lmi-user 10 QKQ1.191117.002 V12.0.4.0.QJKMIXM release-keys";
+        mod_device = "lmipro";
+    }
+
+    // SafetyNet workaround
+    property_override("ro.boot.verifiedbootstate", "green");
+    fingerprint = "google/sunfish/sunfish:11/RP1A.201005.006/6828489:user/release-keys";
+
+    set_ro_build_prop("fingerprint", fingerprint);
+    set_ro_product_prop("device", device);
+    set_ro_product_prop("model", model);
+    property_override("ro.build.description", description.c_str());
+    if (mod_device != "") {
+        property_override("ro.product.mod_device", mod_device.c_str());
     }
 }
