109b1627748aafddd8fdc2846012cb48ece17c8e
vasishath <vasishath@gmail.com>
2020-11-09 04:34:07 +0100

    lmi: light: Detect maximum brightness dynamically
    Change-Id: I9385be40a8598e1142bb3844446b6bbeb5aab008

--- light/Light.cpp
+++ light/Light.cpp
@@ -31,8 +31,7 @@
 #define DELAY_OFF       "delay_off"
 #define DELAY_ON        "delay_on"
 
-#define MAX_LED_BRIGHTNESS    255
-#define MAX_LCD_BRIGHTNESS    2047
+#define MAX_BRIGHTNESS      "max_brightness"
 
 namespace {
 /*
@@ -51,6 +50,22 @@ static void set(std::string path, std::string value) {
 
 static void set(std::string path, int value) {
     set(path, std::to_string(value));
+}
+
+/*
+ * Read max brightness from path and close file.
+ */
+static int getMaxBrightness(std::string path) {
+    std::ifstream file(path);
+    int value;
+
+    if (!file.is_open()) {
+        ALOGW("failed to read from %s", path.c_str());
+        return 0;
+    }
+
+    file >> value;
+    return value;
 }
 
 static uint32_t getBrightness(const LightState& state) {
@@ -87,12 +102,12 @@ static inline uint32_t getScaledBrightness(const LightState& state, uint32_t max
 }
 
 static void handleBacklight(const LightState& state) {
-    uint32_t brightness = getScaledBrightness(state, MAX_LCD_BRIGHTNESS);
+    uint32_t brightness = getScaledBrightness(state, getMaxBrightness(PANEL_LED MAX_BRIGHTNESS));
     set(LCD_LED BRIGHTNESS, brightness);
 }
 
 static void handleNotification(const LightState& state) {
-    uint32_t greenBrightness = getScaledBrightness(state, MAX_LED_BRIGHTNESS);
+    uint32_t greenBrightness = getScaledBrightness(state, getMaxBrightness(NOTIFICATION_LED MAX_BRIGHTNESS));
 
     /* Disable breathing or blinking */
 
